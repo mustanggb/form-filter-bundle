11672e94a98043cd2316b000a56dfa76c3536d23
MustangGB <mustang@miswave.com>
2023-10-02 13:58:26 +0100

    Update MongodbQueryBuilderUpdaterTest.php

--- Tests/Filter/Doctrine/MongodbQueryBuilderUpdaterTest.php
+++ Tests/Filter/Doctrine/MongodbQueryBuilderUpdaterTest.php
@@ -46,10 +46,10 @@ class MongodbQueryBuilderUpdaterTest extends TestCase
         $year = '2019';
 
         $bson = ['{}', '{"$and":[{"name":"blabla"}]}', '{"$and":[{"name":"blabla"},{"position":{"$gt":2}}]}', '{"$and":[{"name":"blabla"},{"position":{"$gt":2}},{"enabled":true}]}', '{"$and":[{"name":"blabla"},{"position":{"$gt":2}},{"enabled":true}]}', [
-            '{"$and":[{"name":{"pattern":".*blabla$","flags":"i"}},{"position":{"$lte":2}},{"createdAt":{"$date":{"$numberLong":"1569535200000"}}}]}',
+            '{"$and":[{"name":{"$regex":".*blabla$","$options":"i"}},{"position":{"$lte":2}},{"createdAt":{"$date":{"$numberLong":"1569535200000"}}}]}',
             '{"$and":[{"name":"\/.*blabla$\/i"},{"position":{"$lte":2}},{"createdAt":{"$date":{"$numberLong":"1569535200000"}}}]}'
         ], [
-            '{"$and":[{"name":{"pattern":".*blabla$","flags":"i"}},{"position":{"$lte":2}},{"createdAt":{"$date":{"$numberLong":"1569583260000"}}}]}',
+            '{"$and":[{"name":{"$regex":".*blabla$","$options":"i"}},{"position":{"$lte":2}},{"createdAt":{"$date":{"$numberLong":"1569583260000"}}}]}',
             '{"$and":[{"name":"\/.*blabla$\/i"},{"position":{"$lte":2}},{"createdAt":{"$date":{"$numberLong":"1569583260000"}}}]}'
         ]];
 
@@ -243,14 +243,14 @@ class MongodbQueryBuilderUpdaterTest extends TestCase
 
         fwrite(STDERR, print_r(json_encode($mongoQB->getQueryArray()), TRUE));
         fwrite(STDERR, print_r([
-            '{"$and":[{"name":{"pattern":".*hey dude.*","flags":"i"}},{"position":99}]}',
+            '{"$and":[{"name":{"$regex":".*hey dude.*","$options":"i"}},{"position":99}]}',
             '{"$and":[{"name":"\/.*hey dude.*\/i"},{"position":99}]}'
         ], TRUE));
 
         $this->assertContains(
             $this->toBson($mongoQB->getQueryArray()),
             [
-                '{"$and":[{"name":{"pattern":".*hey dude.*","flags":"i"}},{"position":99}]}',
+                '{"$and":[{"name":{"$regex":".*hey dude.*","$options":"i"}},{"position":99}]}',
                 '{"$and":[{"name":"\/.*hey dude.*\/i"},{"position":99}]}'
             ]
         );
